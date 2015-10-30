15f322303c1202536085c559b8e1eeeb4d8cb72e
Boyang Niu <boyangniu@yahoo-inc.com>
2015-10-30 16:22:41 -0700

    Fix symlinking

--- setup.rb
+++ setup.rb
@@ -3,14 +3,16 @@ require 'rbconfig'
 require 'shellwords'
 
 subl_config_dir = case RbConfig::CONFIG['host_os']
-    when /mswin|windows/i
-        "#{ENV['APPDATA']}\\Sublime\ Text\ 3/Packages/User"
-    when /darwin/i
-        "#{ENV['HOME']}/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
-    else
-        "#{ENV['HOME']}/.config/sublime-text-3/Packages/User"
-    end
+  when /mswin|windows/i
+      "#{ENV['APPDATA']}\\Sublime\ Text\ 3/Packages/User"
+  when /darwin/i
+      "#{ENV['HOME']}/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
+  else
+      "#{ENV['HOME']}/.config/sublime-text-3/Packages/User"
+  end
+
+current_dir = %x[pwd].strip
 
 Dir["*.sublime*"].each do |filename|
-    %x[ln -fs #{filename.shellescape} #{subl_config_dir.shellescape}/#{File.basename(filename).shellescape}]
+  %x[ln -sf #{File.join(current_dir, filename).shellescape} #{subl_config_dir.shellescape}/#{File.basename(filename).shellescape}]
 end
